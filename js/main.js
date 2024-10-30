5dec700b16889311caaf9a983fc55837d738e8d4
kuro096 <43433044+kuro096@users.noreply.github.com>
2024-10-30 23:54:13 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,14 +5,21 @@ function Time() {
      var monthRow = realTime.getMonth() + 1;
      var month = ('0'+monthRow).slice(-2);
      var day = ('0'+realTime.getDate()).slice(-2);
-     var text2 = year + "年" + month + "月" + day+ "日";
+     var days = ["日","月","火","水","木","金","土",];
+     console.log(days[realTime.getDay()]);
+
+     var text2 = year + "年" + month + "月" + day+ "日(" + days[realTime.getDay()] + ")";
      
      var hour = ('0'+realTime.getHours()).slice(-2);
      var minutes  = ('0'+realTime.getMinutes()).slice(-2);
      var seconds  = ('0'+realTime.getSeconds()).slice(-2);
      var text = hour + ":" + minutes + ":" + seconds;
 
+     var days = ["日","月","火","水","木","金","土",];
+     console.log(days[realTime.getDay()]);
+
+
      document.getElementById("real-time").innerHTML = text;
      document.getElementById("real-date").innerHTML = text2;
   }
-  setInterval('Time()',1000);
+  setInterval('Time()',500);
